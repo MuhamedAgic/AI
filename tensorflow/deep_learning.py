955366336d349f533184d1052b7bb0b2166b4743
Muhamed Agic <38727392+Muhamedhro@users.noreply.github.com>
2022-08-06 13:07:07 +0200

    tensorboard werkt

--- tensorflow/deep_learning.py
+++ tensorflow/deep_learning.py
@@ -3,6 +3,12 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 import datetime
+from tensorflow.python.client import device_lib
+
+
+def get_available_gpus():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos if x.device_type == 'GPU']
 
 
 def use_gpus(): 
@@ -13,6 +19,8 @@ def use_gpus():
         tf.config.set_logical_device_configuration(
             gpus[0],
             [tf.config.LogicalDeviceConfiguration(memory_limit=1024),
+             tf.config.LogicalDeviceConfiguration(memory_limit=1024),
+             tf.config.LogicalDeviceConfiguration(memory_limit=1024),
              tf.config.LogicalDeviceConfiguration(memory_limit=1024)])
         logical_gpus = tf.config.list_logical_devices('GPU')
         print(len(gpus), "Physical GPU,", len(logical_gpus), "Logical GPUs")
@@ -39,6 +47,7 @@ def create_classification_model():
         
 #start
 
+get_available_gpus()
 use_gpus()
 classification_model = create_classification_model()
 
@@ -48,18 +57,20 @@ handwritten_dataset = tf.keras.datasets.mnist  #downloads the mnist dataset and 
 x_train, x_test = x_train / 255.0, x_test / 255.0   
 
 #tensorboard, logging en model statistieken
+#to launch tensorboard: python -m tensorboard.main --logdir=[PATH_TO_LOGDIR]
 log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
 #trainen
 classification_model.fit(x_train, 
                          y_train, 
-                         epochs = 500, 
+                         epochs = 50, 
                          batch_size = 500,
                          callbacks=[tensorboard_callback])
 
 classification_model.evaluate(x_test,  y_test)
 
+#predictions
 predictions=classification_model.predict(x_test)
 print("predicted value =",np.argmax(predictions[0]))
 print("real value =", y_test[0])
